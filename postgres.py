53314a581702913103f9bed5fea53ad816cd278a
guillaume <guillaume@imac-de-guillaume.home>
2018-04-05 23:40:28 +0200

    Added read_from_db function to the script

--- postgres.py
+++ postgres.py
@@ -6,8 +6,10 @@ import psycopg2
 conn = psycopg2.connect("dbname=openfoodfacts user=postgres password=postgres")
 cursor = conn.cursor()
 
+
 def create_category_table():
     cursor.execute("CREATE TABLE IF NOT EXISTS category (id INTEGER PRIMARY KEY, category TEXT, datestamp TEXT);")
+
 
 def data_entry():
     cursor.execute("INSERT INTO category VALUES (0, 'I am a category', '2018-04-05');")
@@ -15,5 +17,15 @@ def data_entry():
     cursor.close()
     conn.close()
 
-create_category_table()
-data_entry()
+
+def read_from_db():
+    cursor.execute("SELECT * FROM category")
+    # data = cursor.fetchall()
+    for row in cursor.fetchall():
+        print(row)
+
+
+# create_category_table()
+# data_entry()
+
+read_from_db()
