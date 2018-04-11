8d586c5f0329362c58496e8378f5df572d8522a3
Guillaume Pont <pont.guillaume@yahoo.fr>
2018-04-11 23:53:39 +0200

    Deleted datestamp from tables schemas

--- create_table.py
+++ create_table.py
@@ -8,19 +8,19 @@ cursor = conn.cursor()
 
 
 def create_category_table():
-    cursor.execute("CREATE TABLE IF NOT EXISTS category (category_id TEXT PRIMARY KEY, category_name_fr TEXT, datestamp TEXT);")
+    cursor.execute("CREATE TABLE IF NOT EXISTS category (category_id TEXT PRIMARY KEY, category_name_fr TEXT);")
 
 
 def create_product_table():
-    cursor.execute("CREATE TABLE IF NOT EXISTS product (product_id INTEGER PRIMARY KEY, category_id INTEGER, product_name_fr TEXT, nutrition_grade_id INTEGER, product_url_fr TEXT, datestamp TEXT);")
+    cursor.execute("CREATE TABLE IF NOT EXISTS product (product_id INTEGER PRIMARY KEY, category_id INTEGER, product_name_fr TEXT, nutrition_grade_id INTEGER, product_url_fr TEXT);")
 
 
 def create_nutrition_grade_table():
-    cursor.execute("CREATE TABLE IF NOT EXISTS nutrition_grade (nutrition_grade_id INTEGER PRIMARY KEY, nutrition_grade_desc TEXT, datestamp TEXT);")
+    cursor.execute("CREATE TABLE IF NOT EXISTS nutrition_grade (nutrition_grade_id INTEGER PRIMARY KEY, nutrition_grade_desc TEXT);")
 
 
 def create_store_table():
-    cursor.execute("CREATE TABLE IF NOT EXISTS store (store_id INTEGER PRIMARY KEY, store_desc_fr TEXT, datestamp TEXT);")
+    cursor.execute("CREATE TABLE IF NOT EXISTS store (store_id INTEGER PRIMARY KEY, store_desc_fr TEXT);")
 
 
 
