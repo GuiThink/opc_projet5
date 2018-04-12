3885dd005d6a7f14fd349b7fd840e03858c75a72
Guillaume Pont <pont.guillaume@yahoo.fr>
2018-04-12 13:28:24 +0200

    Revised script to update data types

--- create_table.py
+++ create_table.py
@@ -8,19 +8,19 @@ cursor = conn.cursor()
 
 
 def create_category_table():
-    cursor.execute("CREATE TABLE IF NOT EXISTS category (category_id TEXT PRIMARY KEY, category_name_fr TEXT);")
+    cursor.execute("CREATE TABLE IF NOT EXISTS category (category_id VARCHAR(100)PRIMARY KEY NOT NULL, category_name_fr VARCHAR(100) NOT NULL);")
 
 
 def create_product_table():
-    cursor.execute("CREATE TABLE IF NOT EXISTS product (product_id INTEGER PRIMARY KEY, category_id INTEGER, product_name_fr TEXT, nutrition_grade_id INTEGER, product_url_fr TEXT);")
+    cursor.execute("CREATE TABLE IF NOT EXISTS product (product_id VARCHAR(100) PRIMARY KEY NOT NULL, category_id VARCHAR(100) NOT NULL, product_name_fr VARCHAR(100) NOT NULL, nutrition_grade_id VARCHAR(14) NOT NULL, product_url_fr TEXT NOT NULL);")
 
 
 def create_nutrition_grade_table():
-    cursor.execute("CREATE TABLE IF NOT EXISTS nutrition_grade (nutrition_grade_id INTEGER PRIMARY KEY, nutrition_grade_desc TEXT);")
+    cursor.execute("CREATE TABLE IF NOT EXISTS nutrition_grade (nutrition_grade_id VARCHAR(14) PRIMARY KEY NOT NULL, nutrition_grade_desc TEXT NOT NULL);")
 
 
 def create_store_table():
-    cursor.execute("CREATE TABLE IF NOT EXISTS store (store_id INTEGER PRIMARY KEY, store_desc_fr TEXT);")
+    cursor.execute("CREATE TABLE IF NOT EXISTS store (store_id TEXT PRIMARY KEY NOT NULL, store_desc_fr TEXT NOT NULL);")
 
 
 
