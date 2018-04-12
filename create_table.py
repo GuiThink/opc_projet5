19e42b433b55bd6687c992dc67f8463a159051f2
Guillaume Pont <pont.guillaume@yahoo.fr>
2018-04-12 17:40:05 +0200

    Modified data types

--- create_table.py
+++ create_table.py
@@ -8,7 +8,7 @@ cursor = conn.cursor()
 
 
 def create_category_table():
-    cursor.execute("CREATE TABLE IF NOT EXISTS category (category_id VARCHAR(100)PRIMARY KEY NOT NULL, category_name_fr VARCHAR(100) NOT NULL);")
+    cursor.execute("CREATE TABLE IF NOT EXISTS category (id SERIAL PRIMARY KEY, category_id TEXT NOT NULL, category_name_fr TEXT NOT NULL);")
 
 
 def create_product_table():
