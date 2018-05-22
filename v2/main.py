c9d7147d140f2af230d5a267a620e93566c9d475
Guillaume Pont <pont.guillaume@yahoo.fr>
2018-05-22 12:37:53 +0200

    Added filters for categories selection before insert

--- v2/main.py
+++ v2/main.py
@@ -6,14 +6,13 @@ import requests
 import json
 import os
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
-import sys
 
 
 def get_api_data():
     """
     Retrieves data from api url
     """
-    api_url = "https://fr.openfoodfacts.org/cgi/search.pl?action=process&tagtype_0=countries&tag_contains_0=contains&tag_0=france&tagtype_1=purchase_places&tag_contains_1=contains&tag_1=france&tagtype_2=languages&tag_contains_2=contains&tag_2=fran%C3%A7ais&sort_by=unique_scans_n&page_size=100&axis_x=energy&axis_y=products_n&action=display&json=1"
+    api_url = "https://fr.openfoodfacts.org/cgi/search.pl?action=process&tagtype_0=countries&tag_contains_0=contains&tag_0=france&tagtype_1=purchase_places&tag_contains_1=contains&tag_1=france&tagtype_2=languages&tag_contains_2=contains&tag_2=fran%C3%A7ais&sort_by=unique_scans_n&page_size=500&axis_x=energy&axis_y=products_n&action=display&json=1"
     json_data = requests.get(api_url).json()
     write_json_file(json_data)
 
@@ -35,7 +34,7 @@ def read_from_local_json_file():
     if os.path.isfile('json_openfoodfacts_db.json'):
         with open('json_openfoodfacts_db.json') as json_data:
             data = json.load(json_data)
-            select_categories(data)
+            filter_categories(data)
 
 
 def create_database():
@@ -69,34 +68,69 @@ def create_table():
     conn.close()
 
 
-def select_categories(data):
+def filter_categories(data):
     """
     Selects fr category id's and apply some filters
     """
     all_categories = []
 
-    for each in data['products']:
-        category = each['categories_tags']
-        all_categories.append(category)
+    for product in data['products']:
+        category_list = product['categories_tags']
+        for category in category_list:
+            all_categories.append(category)
 
     #print(all_categories)
 
+
     all_french_categories = []
 
-    for each in all_categories:
-        for elem in each:
-            if 'fr:' in elem:
-                all_french_categories.append(elem)
+    for category in all_categories:
+        if 'fr:' in category:
+            all_french_categories.append(category)
 
-    print(all_french_categories)
-
-
-def remove_duplicates():
-    pass
+    #print(all_french_categories)
+    remove_empty_categories(all_french_categories)
 
 
-def import_categories():
-    pass
+def remove_empty_categories(category_list):
+    """
+    Makes sure not to select categories with not enough related products in it
+    """
+    keepers = []
+    occurrence_limit = 2
+    for category in category_list:
+        if category_list.count(category) >= occurrence_limit:
+            keepers.append(category)
+
+    #print(keepers)
+    remove_duplicates(keepers)
+
+
+def remove_duplicates(category_list):
+    """
+    Removes duplicates from categories list
+    """
+    clean_french_categories = []
+    check_list = set()
+    for category in category_list:
+        if category not in check_list:
+            clean_french_categories.append(category)
+            check_list.add(category)
+            print(category)
+
+    import_into_categories(clean_french_categories)
+
+
+def import_into_categories(data_to_insert):
+    """
+    Import selected categories into PostgreSql table
+    """
+    data = data_to_insert
+    print(data)
+    with psycopg2.connect(host="localhost", database="openfoodfacts_db", user="postgres", password="postgres") as conn:
+        with conn.cursor() as cursor:
+            cursor.executemany("INSERT INTO category (category_id) VALUES (%s)", data)
+        conn.commit()
 
 
 def import_stores():
