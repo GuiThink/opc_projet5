b766e9f2ee6d26db3174cb62e72fcbeee7292649
Guillaume Pont <pont.guillaume@yahoo.fr>
2018-06-05 17:35:13 +0200

    Revised project structure

---
+++ create_database.py
@@ -0,0 +1,52 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+
+def create_database():
+    """
+    Database creation process
+    """
+    try:
+        conn = psycopg2.connect(host="localhost", user="postgres", password="postgres")
+        conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+        cursor = conn.cursor()
+        cursor.execute("CREATE DATABASE openfoodfacts_db ENCODING 'UTF8';")
+        conn.commit()
+        cursor.close()
+        conn.close()
+    except psycopg2.ProgrammingError:
+        pass
+
+
+def create_table():
+    """
+    Tables creation process
+    """
+    conn = psycopg2.connect(host="localhost", database="openfoodfacts_db", user="postgres", password="postgres")
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS category (category_id VARCHAR(255) UNIQUE NOT NULL, PRIMARY KEY(category_id));")
+
+    cursor.execute("CREATE TABLE IF NOT EXISTS store (store_id VARCHAR(255) UNIQUE NOT NULL, PRIMARY KEY (store_id));")
+
+    cursor.execute("""CREATE TABLE IF NOT EXISTS product 
+                      (product_id VARCHAR(255) UNIQUE NOT NULL,
+                      product_name_fr VARCHAR(255) NOT NULL,
+                      product_nutrition_grade VARCHAR(1) NOT NULL,
+                      product_url VARCHAR(255) NOT NULL,
+                      store_id VARCHAR(255) NOT NULL REFERENCES store (store_id),
+                      category_id VARCHAR(255) NOT NULL REFERENCES category (category_id),
+                      PRIMARY KEY (product_id)
+                      );""")
+
+    cursor.execute("""CREATE TABLE IF NOT EXISTS history 
+                      (product_id VARCHAR(255) NOT NULL REFERENCES product (product_id), 
+                      date_time TIMESTAMP NOT NULL);""")
+
+    conn.commit()
+    cursor.close()
+    conn.close()
